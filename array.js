534a8986aded0c7d8ea434f90dc69eb85bb50232
nijmi <nijmibaj@gmail.com>
2026-07-06 19:07:18 +0545

    first commit

---
+++ array.js
@@ -0,0 +1,62 @@
+//ARRAY IN JS
+/*
+let n=[1,2,3,4,5,6];
+n.push(10); //add element to the end
+console.log(n);
+
+let n=[1,2,3,4,5,6];
+n.pop(); //remove element from the end
+console.log(n);
+
+let n=[1,2,3,4,5,6];
+n.shift(); //remove the first element
+console.log(n);
+
+let n=[1,2,3,4,5,6];
+n.unshift(22); //adds the first element
+console.log(n);
+
+let n=[1,2,3,4,5,6];
+console.log(n.length);//finds length
+
+let n=[1,2,3,4,5,6];
+console.log(n.indexOf(4));//finds index of element
+
+let n=[1,2,3,4,5,6];
+console.log(n.includes(5));//checks if element exist or not
+
+let n=[1,2,3,4,5,6];
+console.log(n.slice(3,5));//copy array
+
+let n=[1,2,3,4,5,6];
+console.log(n.splice(2,1));//add or remove elements 
+
+let n=[1,2,3,4,5,6];
+console.log(n.reverse());//reverse the array
+
+let n1=[1,2,3,4];
+let n2=[5,6,7,8];
+let r=n1.concat(n2); //join arrays
+console.log(r);
+
+let n=[1,2,3,4];
+n.forEach(function(num){ //visit each element
+    console.log(num);
+});
+
+let n=[1,2,3,4];
+let triple=n.map(num=>num*3);//create new array
+console.log(triple);
+
+let n=[1,2,3,4];
+let r=n.filter(num=>num > 2);//select array
+console.log(r);
+
+let n=[1,2,3,4];
+let r=n.find(num=>num > 3);//find array
+console.log(r);
+
+let n=[1,2,3,4];
+let sum=n.reduce((total,num)=>total+num,0);//single value
+console.log(sum);
+*/
